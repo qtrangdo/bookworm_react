bd28fc6a1f10cf5bc6beee460b8bc30603d5db68
td94 <quynhtrangdo94@gmail.com>
2019-01-06 17:16:04 -0800

    Update LoginPage and reducers

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import LoginForm from '../forms/LoginForm'
+import LoginForm from '../forms/LoginForm';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
 class LoginPage extends Component {
 
     submit = (data) => {
-        console.log(data);
+        this.props.login(data)
+        .then(() => this.props.history.push('/'));
     }
     render() {
         return (
@@ -16,4 +20,10 @@ class LoginPage extends Component {
     }
 };
 
-export default LoginPage;
+LoginPage.propTypes = {
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired
+    }).isRequired,
+    login: PropTypes.func.required
+}
+export default connect(null, { login })(LoginPage);
