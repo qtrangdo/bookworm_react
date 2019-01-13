fe3d15ab1b74bde88f13e126d4d05f6d6f8f8ae8
td94 <quynhtrangdo94@gmail.com>
2019-01-13 01:16:45 -0800

    Implement login/logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,11 +1,24 @@
-import { USER_LOGGED_IN } from '../types';
+import { USER_LOGGED_IN, USER_LOGGED_OUT } from '../types';
 import api from '../api';
 
 export const userLoggedIn = (user) => ({
     type: USER_LOGGED_IN,
     user
 })
+export const userLoggedOut = () => ({
+    type: USER_LOGGED_OUT
+})
+
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('bookwormJWT')
+    dispatch(userLoggedOut());
+}
+    
 
 export const login = (credentials) => (dispatch) =>
 (api.user.login(credentials))
-.then(user => dispatch(userLoggedIn(user)))
+    .then(user => {
+        localStorage.bookwormJWT = user.token;
+        dispatch(userLoggedIn(user));
+    })
--- src/index.js
+++ src/index.js
@@ -9,10 +9,16 @@ import * as serviceWorker from './serviceWorker';
 import rootReducer from './rootReducer';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { userLoggedIn } from './actions/auth';
 
 const store = createStore(
     rootReducer, composeWithDevTools(applyMiddleware(thunk))
 );
+
+if (localStorage.bookwormJWT) {
+    const user = { token: localStorage.bookwormJWT };
+    store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(
     <BrowserRouter>
