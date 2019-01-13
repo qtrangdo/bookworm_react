208f3dac9e74c9b7c139780cff1eedc9aa0d6dd5
td94 <quynhtrangdo94@gmail.com>
2019-01-13 13:10:25 -0800

    Connect signup function to rest api

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
+import SignupPage from './components/pages/SignupPage';
 import Dashboard from './components/pages/Dashboard';
-import UserRoute from './components/routes/UserRoute'
-import GuestRoute from './components/routes/GuestRoute'
+import UserRoute from './components/routes/UserRoute';
+import GuestRoute from './components/routes/GuestRoute';
 
 const App = ({ location }) => (
   <div className="ui container">
     <Route location={location} path = "/" exact component={HomePage} />
     <GuestRoute location={location} path = "/login" exact component={LoginPage} />
+    <GuestRoute location={location} path = "/signup" exact component={SignupPage} />
     <UserRoute location={location} path = "/dashboard" exact component={Dashboard} />
   </div>
 );
